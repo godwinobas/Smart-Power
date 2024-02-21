82ba790d1c33e871422781e0baf2b79c298b2fc6
iamMusa <godwinobas0@gmail.com>
2024-02-21 09:21:23 +0100

    added check user function

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const AUTHMESSAGE = 'Kindly signup or login if you already have an account';
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -8,15 +10,35 @@ const requireAuth = (req, res, next) => {
     jwt.verify(token, 'gordons the badass coder', (err, decodedToken) => {
       if (err) {
         console.log(err.message);
-        res.redirect('/login');
+        res.status(401).json({ AUTHMESSAGE });
       } else {
         console.log(decodedToken);
         next();
       }
     });
   } else {
-    const authMessage = 'Kindly signup or login if you already have an account';
-    res.status(401).json({ authMessage });
+    res.status(401).json({ AUTHMESSAGE });
   }
 };
+
+//  check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'gordons the badass coder', async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        next();
+      }
+    });
+    console.log(err.message);
+    next();
+  }
+};
+
 module.exports = { requireAuth };
